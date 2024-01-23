6396e1422bb762abc9723b7b5d2927ec79ea5c39
Renee Camillo Pollet <renee_p_35@hotmail.com>
2024-01-23 10:34:21 -0700

    King Happiness Script

--- Assets/Scripts/king/KingHappinessManager.cs
+++ Assets/Scripts/king/KingHappinessManager.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class KingHappinessManager : MonoBehaviour
 {
-    //test
+    private int kingHappiness;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        kingHappiness = 5;
+    }
+
+    public int _kingHappiness   // property
+    {
+        get { return kingHappiness; }   // get method
+        set { kingHappiness = value; }  // set method
+    }
+
+    public void AddHappiness(int addedPoints)
+    {
+        kingHappiness += addedPoints;
     }
 
     // Update is called once per frame
