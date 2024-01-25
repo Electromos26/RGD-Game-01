1f07c460ab5487d117068cf38ce632fa0b4c911b
Mos <musse.hidru@outlook.com>
2024-01-24 23:12:24 -0700

    Dancing Progress ( code done )
    needs animation of play dancing

--- Assets/Scripts/miniGames/Dancing/DanceMiniGame.cs
+++ Assets/Scripts/miniGames/Dancing/DanceMiniGame.cs
@@ -1,26 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DanceMiniGame : Singleton<DanceMiniGame>
 {
- 
     [SerializeField]
-    private int tries = 3;
+    private int turnLeft = 3;
     [SerializeField]
     private BallMovement ball;
+
+    private int finalPoints = 0;
+
     [SerializeField]
-    private SkillCheck[] skillBoxes;
-    int currentBox = 0;
+    private GameObject skillCheckGame;
+
+    private bool isDancing = false;
+    public bool IsDancing { get { return isDancing; } set { isDancing = value; } }
+
 
     private void Awake()
     {
-       
+        
+    }
+     
+    public void StartDancing()
+    {
+        isDancing = true;
+        skillCheckGame.SetActive(true);
+
+       //enable skill check
+    }
+    void StopDancing()
+    {
+        isDancing = false;
+
+        //disable skill check
     }
     public void NextTurn()
     {
-      ball.SpeedX2();
-        currentBox++;
-    } 
-    
+        skillCheckGame.SetActive(false);
+        StopDancing();
+        if (turnLeft > 0)
+        {
+            turnLeft--;
+        }
+        ball.SpeedX2();
+        //show animation 
+        Invoke("StartDancing", 3f); //use Aniamtion event to call this
+
+
+    }
+
+    public void Passed()
+    {
+   if (finalPoints < 2)
+        {
+            NextTurn();
+            finalPoints++;
+           
+        }
+    }
+    public void Failed()
+    {
+   if (finalPoints > -2)
+        {
+            NextTurn();
+            finalPoints--;
+        }
+    }
+ 
+
+   
+    private void Update()
+    {
+        if (turnLeft <= 0)
+        {
+            FinishMiniGame();
+        }
+    }
+    public void FinishMiniGame()
+    {
+        KingHappinessManager.Instance.AddHappiness(finalPoints);
+
+        TransitionManager.Instance.SetState(TransitionManager.State.KingReacting);
+    }
 }
--- Assets/Scripts/miniGames/Dancing/SkillCheck.cs
+++ Assets/Scripts/miniGames/Dancing/SkillCheck.cs
@@ -2,18 +2,29 @@ using UnityEngine;
 
 public class SkillCheck : MonoBehaviour
 {
+    [SerializeField]
+    private int left = -200;
+    [SerializeField]
+    private int right = 270;
+
+    RectTransform rect;
+    RectTransform originalPos;
     bool pass = false;
+
+    bool canPress = false;
 
     private void Awake()
     {
+       rect = GetComponent<RectTransform>();
+       originalPos = rect;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-       
+
         if (collision.gameObject.CompareTag("SkillBox"))
         {
             pass = true;
-           
+
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -28,16 +39,50 @@ public class SkillCheck : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+
             if (pass)
             {
                 Debug.Log("Skill Check : Passed");
-               // DanceMiniGame.Instance.NextTurn();
+                ChangePos();
+                DanceMiniGame.Instance.Passed();
+                //Telepot to next position
             }
-            else
+            else if(canPress)
             {
+                ChangePos();
                 Debug.Log("Skill Check : Failed");
+                DanceMiniGame.Instance.Failed();
+            } else
+            {
+                Debug.Log("Skill Check : Waiting");
             }
         }
     }
+
+    private void OnEnable()
+    {
+        canPress = true;  
+    }
+    private void OnDisable()
+    {
+        canPress = false;
+    }
+    private void ChangePos()
+    {
+        Vector2 randomPos;
+        randomPos.x = Random.Range(left, right);
+        Debug.Log("X = " + randomPos.x);
+        randomPos.y = rect.localPosition.y;
+        Debug.Log( "Y = "+ randomPos.y);
+        
+        rect.localPosition = randomPos;
+    }
+
+    public void ResetPos()//Called when mini game is over
+    {
+        rect.position = originalPos.position;
+    }
+
+
 
 }
